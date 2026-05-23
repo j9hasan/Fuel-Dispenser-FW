5550de707dd29a6bf010095b201fdd719b073758
jubaid <walton.fw02@gmail.com>
2026-05-23 18:21:01 +0600

    initial protocol driver developed

--- FW/fdisp/Core/Inc/genuine_rs485.h
+++ FW/fdisp/Core/Inc/genuine_rs485.h
@@ -13,7 +13,7 @@
 #include <stdbool.h>
 
 /* Configuration */
-#define RS485_RX_BUFFER_SIZE      8
+#define RS485_RX_BUFFER_SIZE      124
 #define RS485_TX_TIMEOUT_MS       100
 #define RS485_RX_TIMEOUT_MS       200
 
@@ -27,89 +27,64 @@
 #define DISP_HEADER               0xA5
 
 /* Function codes */
-typedef enum
-{
-    DISP_FUNC_READ      = 0x03,
-    DISP_FUNC_WRITE     = 0x10,
-    DISP_FUNC_EVENT     = 0x0C
+typedef enum {
+	DISP_FUNC_READ = 0x03, DISP_FUNC_WRITE = 0x10, DISP_FUNC_EVENT = 0x0C
 
-}DispFunction_t;
+} DispFunction_t;
 
 /* Origin address */
-typedef enum
-{
-    DISP_STATUS         = 0x00,
-    DISP_PRESET_LITER   = 0x10,
-    DISP_PRESET_SALE    = 0x14,
-    DISP_REALTIME       = 0x20,
-    DISP_CURRENT        = 0x30,
-    DISP_PRICE          = 0x55,
-    DISP_MODE           = 0x5A,
-    DISP_TOTAL          = 0x60,
-    DISP_CLASS_TOTAL    = 0x70,
-    DISP_OFFLINE        = 0x80
+typedef enum {
+	DISP_STATUS = 0x00,
+	DISP_PRESET_LITER = 0x10,
+	DISP_PRESET_SALE = 0x14,
+	DISP_REALTIME = 0x20,
+	DISP_CURRENT = 0x30,
+	DISP_PRICE = 0x55,
+	DISP_MODE = 0x5A,
+	DISP_TOTAL = 0x60,
+	DISP_CLASS_TOTAL = 0x70,
+	DISP_OFFLINE = 0x80
 
-}DispOrigin_t;
+} DispOrigin_t;
 
-typedef struct
-{
-    UART_HandleTypeDef *uart;
+typedef struct {
+	UART_HandleTypeDef *uart;
 
-    GPIO_TypeDef *dePort;
-    uint16_t dePin;
+	GPIO_TypeDef *dePort;
+	uint16_t dePin;
 
-    uint8_t rxByte;
+	uint8_t rxBuf[RS485_RX_BUFFER_SIZE];
+	uint16_t rxLen;
+	volatile bool rxDone;
 
-    uint8_t rxBuf[RS485_RX_BUFFER_SIZE];
-    uint16_t rxIndex;
-
-    volatile bool rxDone;
-
-}RS485_Handle_t;
-
+} RS485_Handle_t;
 
 /* Init */
-void RS485_Init(
-        RS485_Handle_t *h,
-        UART_HandleTypeDef *uart,
-        GPIO_TypeDef *dePort,
-        uint16_t dePin);
+void RS485_Init(RS485_Handle_t *h, UART_HandleTypeDef *uart,
+		GPIO_TypeDef *dePort, uint16_t dePin);
 
 /* TX/RX */
-HAL_StatusTypeDef RS485_Send(
-        RS485_Handle_t *h,
-        uint8_t *data,
-        uint16_t len);
+HAL_StatusTypeDef RS485_Send(RS485_Handle_t *h, uint8_t *data, uint16_t len);
 
-void RS485_StartReceive(
-        RS485_Handle_t *h);
+void RS485_StartReceive(RS485_Handle_t *h);
 
-void RS485_RxCallback(
-        RS485_Handle_t *h);
+void RS485_RxCallback(RS485_Handle_t *h);
 
 /* Packet helpers */
-uint16_t Disp_BuildRead(
-        uint8_t addr,
-        uint8_t origin,
-        uint8_t len,
-        uint8_t *txBuf);
+uint16_t Disp_BuildRead(uint8_t addr, uint8_t origin, uint8_t len,
+		uint8_t *txBuf);
 
-uint16_t Disp_BuildWrite(
-        uint8_t addr,
-        uint8_t origin,
-        uint8_t dataLen,
-        uint8_t *payload,
-        uint8_t *txBuf);
+uint16_t Disp_BuildWrite(uint8_t addr, uint8_t origin, uint8_t dataLen,
+		uint8_t *payload, uint8_t *txBuf);
 
 /* Parser */
-bool Disp_ParsePacket(
-        uint8_t *buf,
-        uint16_t len);
+bool Disp_ParsePacket(uint8_t *buf, uint16_t len);
 
 /* CRC */
-uint8_t Disp_CRC8(
-        uint8_t *buf,
-        uint16_t len);
+uint8_t Disp_CRC8(uint8_t *buf, uint16_t len);
 
+/* Read Data */
+//void Disp_GetFuelingRealtime(void);
+//void Disp_ReadStatus(void);
 
 #endif /* INC_GENUINE_RS485_H_ */
